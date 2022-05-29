1f6ab78fa368dd15dfd4fd1c3e353ee67631b636
Mikitatek <mikitatek@gmail.com>
2022-05-29 21:49:48 +0300

    BugFix

---
+++ Tema5TAP/Tema5TAP/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema5TAP
+{
+    public partial class FileExplorer : Form
+    {
+        public FileExplorer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+                webBrowser.GoBack();
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

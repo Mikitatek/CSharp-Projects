82f3eedb75c494a917c24f684a68efa149402b9e
Mikitatek <92375917+Mikitatek@users.noreply.github.com>
2022-05-29 18:51:26 +0300

    Tema1 completa, Tema2 Incompleta

---
+++ Tema1TAP/Tema1TAP/Main.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tema1TAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Adunare");
+            Console.WriteLine("2.Inmultire");
+            Console.WriteLine("3.Inmultire cu un scalar");
+            Console.WriteLine("4.Exit");
+            //Console.WriteLine(args[0]);
+            int input = 0;
+            while (input <= 0 || input > 4)
+            input = GetNumar();
+            switch (input)
+            {
+                case 1:
+                        Adunare();
+                        break;
+                case 2:
+                        Inmultire();
+                        break;
+                case 3:
+                        InmultireScalar();
+                        break;
+                case 4:
+                        System.Environment.Exit(0);
+                        break;
+            }
+
+            int[,] CreazaMatrice()
+            {
+                Console.WriteLine("Input i:");
+                int m = GetNumar();
+                Console.WriteLine("Input j:");
+                int n = GetNumar();
+                int[,] matrice = new int[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.WriteLine("matrice[" + i + "," + j + "]=");
+                        matrice[i, j] = GetNumar();
+                    }
+                    Console.WriteLine("\n");
+                }
+                return matrice;
+            }
+
+            void Adunare()
+            {
+                Console.WriteLine("Matricea 1:");
+                int[,] a = CreazaMatrice();
+                Console.WriteLine("Matricea 2:");
+                int[,] b = CreazaMatrice();
+                Console.WriteLine("Suma Matricelor:");
+                if (a.Length == b.Length)
+                {
+                    int[,] c = new int[a.GetLength(0), b.GetLength(1)];
+                    for (int i = 0; i < a.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < b.GetLength(1); j++)
+                        {
+                            c[i, j] = a[i, j] + b[i, j];
+                            Console.Write(c[i, j] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                    Console.WriteLine("Matricile nu sunt egale.");
+            }
+            
+            void Inmultire()
+            {
+                Console.WriteLine("Matricea 1:");
+                int[,] a = CreazaMatrice();
+                Console.WriteLine("Matricea 2:");
+                int[,] b = CreazaMatrice();
+                Console.WriteLine("Produsul matricelor:");
+                if (a.GetLength(1) == b.GetLength(0))
+                {
+                    int[,] c = new int[a.GetLength(0), b.GetLength(1)];
+                    for (int i = 0; i < a.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < b.GetLength(1); j++)
+                        {
+                            c[i, j] = 0;
+                            for (int k = 0; k < a.GetLength(1); k++)
+                            {
+                                c[i, j] += a[i, k] * b[k, j];
+                            }
+                        }
+                    }
+                    for (int i = 0; i < a.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < a.GetLength(1); j++)
+                        {
+                            Console.Write(c[i, j] + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else 
+                Console.WriteLine("Multiplicarea matricelor nu e posibila");
+            }
+
+            void InmultireScalar()
+            {
+                Console.WriteLine("Matricea:");
+                int[,] a = CreazaMatrice();
+                Console.WriteLine("Numar scalar:");
+                int nr = GetNumar();
+                Console.WriteLine("Inmultirea matricei cu un scalar:");
+                int[,] r = new int[a.GetLength(0), a.GetLength(1)];
+                for(int i = 0; i < a.GetLength(0); i++)
+                    for(int j = 0; j < a.GetLength(1); j++)
+                        r[i, j] = a[i, j]*nr;
+                for (int i = 0; i < r.GetLength(0); i++)
+                {
+                    for (int j = 0; j < r.GetLength(1); j++)
+                    {
+                        Console.Write(r[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            int GetNumar()
+            {
+                bool isNumar = false;
+                string str = String.Empty;
+                while (!isNumar) {
+                    str = Console.ReadLine();
+                    int numericValue;
+                    isNumar = int.TryParse(str, out numericValue);
+                }
+                int numar = Convert.ToInt32(str);
+                if (numar == -1)
+                    System.Environment.Exit(0);
+                return numar;
+            }
+        }
+    }
+}
---
+++ Tema2TAP/Tema2TAP/Program.cs
@@ -0,0 +1,364 @@
+﻿//Studiati felul in care a fost implementata o clasa pentru lucrul cu numere rationale.
+//Apoi, implementati o clasa pentru lucrul cu numere complexe.
+//Clasa model – Numere rationale
+
+using System; 
+using System.Collections.Generic; 
+using System.Linq; using System.Text;
+
+namespace NumberFormats
+{
+    public class Rational
+    {
+        #region Constructors
+        public Rational()
+        {
+            this.Denominator = 1;
+            this.Numerator = 0;
+            this.WholePart = 0;
+        }
+        public Rational(int numerator, int denominator)
+        {
+            this.m_numerator = numerator;
+            this.m_denominator = denominator;
+        }
+        #endregion Constructors
+
+        #region Operators
+        public static Rational operator +(Rational A, Rational B)
+        {
+            return new Rational(A.Numerator * B.Denominator + B.Numerator * A.Denominator, A.Denominator * B.Denominator);
+        }
+
+        public static Rational operator -(Rational A, Rational B)
+        {
+            return new Rational(A.Numerator * B.Denominator + B.Numerator * A.Denominator, A.Denominator * B.Denominator);
+        }
+
+        public static Rational operator *(Rational A, Rational B)
+        {
+            return new Rational(A.Numerator * B.Numerator, A.Denominator * B.Denominator);
+        }
+
+        public static Rational operator /(Rational A, Rational B)
+        {
+            return new Rational(A.Numerator * B.Denominator, B.Numerator * A.Denominator);
+        }
+        public static bool operator ==(Rational A, Rational B)
+        {
+            return (A.Denominator == B.Denominator && A.Numerator == B.Numerator);
+        }
+
+        public static bool operator !=(Rational A, Rational B)
+        {
+            return (A.Denominator != B.Denominator || A.Numerator != B.Numerator);
+        }
+
+        public static bool operator <(Rational A, Rational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) < (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator >(Rational A, Rational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) < (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator <=(Rational A, Rational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) <= (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator >=(Rational A, Rational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) >= (double)(B.Numerator / B.Denominator));
+        }
+
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+        public override bool Equals(System.Object obj)
+#pragma warning restore CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion operators
+
+        #region Methods
+        public override string ToString()
+        {
+            return String.Format("{0} / {1}", this.Numerator, this.Denominator);
+        }
+
+        public static Rational Simplify(Rational x)
+        {
+            int den = x.Denominator;
+            int num = x.Numerator; int divisor = den; // Start division by denominator value
+            while (divisor > 2)
+            {
+                if (num % divisor == 0 && den % divisor == 0)
+                { // Condition true if num and den are divisable by same value
+                    num /= divisor;
+                    den /= divisor;
+                }
+                divisor--;
+            }
+            return new Rational(num, den);
+        }
+
+        public static string StrFraction(double x)
+        {
+            double entero, dec;
+            double divisor;
+            double numerador, denominador;
+            int iteration = 0;
+            entero = (int)x;
+            dec = x - entero;
+            numerador = dec * DECIMALS;
+            denominador = DECIMALS;
+            divisor = 2;
+            while (divisor <= numerador)
+            {
+                if (numerador % divisor == 0 && denominador % divisor == 0)
+                {
+                    numerador /= divisor;
+                    denominador /= divisor;
+                }
+                else divisor++;
+                iteration++;
+            }
+            if (entero == 0)
+                return String.Format("{0}/{1}", numerador, denominador);
+            else return String.Format("{0} - {1}/{2}", entero, numerador, denominador);
+        }
+        public static double ToDecimal(Rational x)
+        {
+            double divisor = x.Numerator / x.Denominator;
+            return divisor;
+        }
+        #endregion
+
+        #region Properties
+        public int Numerator
+        {
+            get
+            {
+                return this.m_numerator;
+            }
+            set
+            {
+                this.m_numerator = value;
+            }
+        }
+        public int Denominator
+        {
+            get
+            {
+                return this.m_denominator;
+            }
+            set
+            {
+                this.m_denominator = value;
+            }
+        }
+        public int WholePart
+        {
+            get
+            {
+                return this.m_WholePart;
+            }
+            set
+            {
+                this.m_WholePart = value;
+            }
+        }
+        #endregion
+
+        #region Fields
+        public const int DECIMALS = 100000; // Value to star the denominator division to simplify
+        private int m_numerator;
+        private int m_denominator;
+        private int m_WholePart;
+        #endregion
+    }
+
+    public class Irational
+    {
+        #region Constructors
+        public Irational()
+        {
+            this.Denominator = 1;
+            this.Numerator = 0;
+            this.WholePart = 0;
+        }
+        public Irational(int numerator, int denominator)
+        {
+            this.m_numerator = numerator;
+            this.m_denominator = denominator;
+        }
+        #endregion Constructors
+
+        #region Operators
+        public static Irational operator +(Irational A, Irational B)
+        {
+            return new Irational(A.Numerator * B.Denominator + B.Numerator * A.Denominator, A.Denominator * B.Denominator);
+        }
+
+        public static Irational operator -(Irational A, Irational B)
+        {
+            return new Irational(A.Numerator * B.Denominator + B.Numerator * A.Denominator, A.Denominator * B.Denominator);
+        }
+
+        public static Irational operator *(Irational A, Irational B)
+        {
+            return new Irational(A.Numerator * B.Numerator, A.Denominator * B.Denominator);
+        }
+
+        public static Irational operator /(Irational A, Irational B)
+        {
+            return new Irational(A.Numerator * B.Denominator, B.Numerator * A.Denominator);
+        }
+        public static bool operator ==(Irational A, Irational B)
+        {
+            return (A.Denominator == B.Denominator && A.Numerator == B.Numerator);
+        }
+
+        public static bool operator !=(Irational A, Irational B)
+        {
+            return (A.Denominator != B.Denominator || A.Numerator != B.Numerator);
+        }
+
+        public static bool operator <(Irational A, Irational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) < (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator >(Irational A, Irational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) < (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator <=(Irational A, Irational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) <= (double)(B.Numerator / B.Denominator));
+        }
+
+        public static bool operator >=(Irational A, Irational B)
+        {
+            return ((double)(A.Numerator / A.Denominator) >= (double)(B.Numerator / B.Denominator));
+        }
+
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+        public override bool Equals(System.Object obj)
+#pragma warning restore CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion operators
+
+        #region Methods
+        public override string ToString()
+        {
+            return String.Format("{0} / {1}", this.Numerator, this.Denominator);
+        }
+
+        public static Irational Simplify(Irational x)
+        {
+            int den = x.Denominator;
+            int num = x.Numerator; int divisor = den; // Start division by denominator value
+            while (divisor > 2)
+            {
+                if (num % divisor == 0 && den % divisor == 0)
+                { // Condition true if num and den are divisable by same value
+                    num /= divisor;
+                    den /= divisor;
+                }
+                divisor--;
+            }
+            return new Irational(num, den);
+        }
+
+        public static string StrFraction(double x)
+        {
+            double entero, dec;
+            double divisor;
+            double numerador, denominador;
+            int iteration = 0;
+            entero = (int)x;
+            dec = x - entero;
+            numerador = dec * DECIMALS;
+            denominador = DECIMALS;
+            divisor = 2;
+            while (divisor <= numerador)
+            {
+                if (numerador % divisor == 0 && denominador % divisor == 0)
+                {
+                    numerador /= divisor;
+                    denominador /= divisor;
+                }
+                else divisor++;
+                iteration++;
+            }
+            if (entero == 0)
+                return String.Format("{0}/{1}", numerador, denominador);
+            else return String.Format("{0} - {1}/{2}", entero, numerador, denominador);
+        }
+        public static double ToDecimal(Irational x)
+        {
+            double divisor = x.Numerator / x.Denominator;
+            return divisor;
+        }
+        #endregion
+
+        #region Properties
+        public int Numerator
+        {
+            get
+            {
+                return this.m_numerator;
+            }
+            set
+            {
+                this.m_numerator = value;
+            }
+        }
+        public int Denominator
+        {
+            get
+            {
+                return this.m_denominator;
+            }
+            set
+            {
+                this.m_denominator = value;
+            }
+        }
+        public int WholePart
+        {
+            get
+            {
+                return this.m_WholePart;
+            }
+            set
+            {
+                this.m_WholePart = value;
+            }
+        }
+        #endregion
+
+        #region Fields
+        public const int DECIMALS = 100000; // Value to star the denominator division to simplify
+        private int m_numerator;
+        private int m_denominator;
+        private int m_WholePart;
+        #endregion
+    }
+}
